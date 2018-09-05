e697f25dbec6ac8482fdab89502fdc80dc872b21
anshul <its-Anshul>
2018-09-05 11:11:56 +0530

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');module.exports = {
  entry: './index.js',
  output: {
-   path: path.resolve(__dirname, 'dist'),
+   path: path.resolve(__dirname, 'docs'),
    filename: 'bundle.js'
  },
  module: {
